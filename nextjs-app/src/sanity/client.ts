69555de152f267d4c3bbda6b3ef65bd8f26fcac1
Ola Lømo Ellingsen <olalel@stud.ntnu.no>
2025-07-19 13:54:14 +0200

    update env

--- nextjs-app/src/sanity/client.ts
+++ nextjs-app/src/sanity/client.ts
@@ -3,7 +3,7 @@ import imageUrlBuilder from "@sanity/image-url";
 import { SanityImageSource } from "@sanity/image-url/lib/types/types";
 
 export const client = createClient({
-  projectId: "1p9yaeef",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "",
   dataset: "production",
   apiVersion: "2024-01-01",
   useCdn: false,
