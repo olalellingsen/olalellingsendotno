a40e0d0d660ef7b32ba5747cb225d478cc6de9b0
Ola Lømo Ellingsen <olalel@stud.ntnu.no>
2025-07-19 14:05:49 +0200

    update config

--- nextjs-app/next.config.ts
+++ nextjs-app/next.config.ts
@@ -5,6 +5,11 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [{ protocol: "https", hostname: "cdn.sanity.io" }],
   },
+  env: {
+    SANITY_PROJECT_ID: process.env.SANITY_PROJECT_ID,
+    SANITY_DATASET: process.env.SANITY_DATASET,
+    SANITY_API_VERSION: process.env.SANITY_API_VERSION,
+  },
 };
 
 export default nextConfig;
--- nextjs-app/src/sanity/client.ts
+++ nextjs-app/src/sanity/client.ts
@@ -4,8 +4,8 @@ import { SanityImageSource } from "@sanity/image-url/lib/types/types";
 
 export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "",
-  dataset: "production",
-  apiVersion: "2024-01-01",
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2024-01-01",
   useCdn: false,
 });
 
